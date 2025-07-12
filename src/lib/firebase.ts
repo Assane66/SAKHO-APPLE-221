f552728307c5501dafa86422da30e2a7b2e1b1c6
Alhassane Ba <baalhassane521@gmail.com>
2025-07-12 12:06:48 +0000

    Maintenant rester connecté toujours jusqu'à que je clique sur déconnecte

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -16,6 +16,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
+
 const db = getFirestore(app);
 const storage = getStorage(app);
 
