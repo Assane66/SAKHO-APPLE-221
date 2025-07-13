43bd2f1928ac3e7fd6a89099b5cea7a3edbdef40
Alhassane Ba <baalhassane521@gmail.com>
2025-07-13 01:04:33 +0000

    enlever voir voir les produits du page acceuil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,11 +59,6 @@ export default async function Home() {
                     <ArrowRight className="ml-2 h-5 w-5" />
                   </Button>
                 </Link>
-                <Link href="/products">
-                  <Button size="lg" variant="outline">
-                    Voir les produits
-                  </Button>
-                </Link>
               </div>
             </div>
             <div className="mx-auto w-full lg:order-last">
