97e3fc56af10f02363ed36c0194b3ac5ec4585c3
Assane Sakho <baalhassane521@gmail.com>
2026-07-21 22:43:22 +0100

    final projet

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCSEIftELB1fPvQ6wVogoUSlKIwWy-bkgA",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "khalil-premium.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "khalil-premium",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "khalil-premium.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "379342311651",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:379342311651:web:4e495393564f3fe2b3f5d6",
+  apiKey: "AIzaSyCSEIftELB1fPvQ6wVogoUSlKIwWy-bkgA",
+  authDomain: "khalil-premium.firebaseapp.com",
+  projectId: "khalil-premium",
+  storageBucket: "khalil-premium.firebasestorage.app",
+  messagingSenderId: "379342311651",
+  appId: "1:379342311651:web:4e495393564f3fe2b3f5d6"
 };
 
 // Initialize Firebase
@@ -19,7 +19,7 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
 if (typeof window !== 'undefined') {
-  setPersistence(auth, browserLocalPersistence).catch(() => {});
+  setPersistence(auth, browserLocalPersistence).catch(() => { });
 }
 
 const db = getFirestore(app);
